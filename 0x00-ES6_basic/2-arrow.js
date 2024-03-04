91090b7809cd02ae5f07b1155fc7f717fcf68862
Kevin Warui <waruimacharia@gmail.com>
2024-03-04 21:00:06 +0000

    Add 3-default-parameter.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,8 +1,8 @@
-export default getNeighborhoodsList() => {
+export default () => {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   this.addNeighborhood = function add(newNeighborhood) {
-    self.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return self.sanFranciscoNeighborhoods;
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
   };
 }
