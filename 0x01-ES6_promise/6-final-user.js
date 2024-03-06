80a7539771f5eefd0f0d27a72e62c49e34c1ce44
Kevin Warui <waruimacharia@gmail.com>
2024-03-06 21:43:27 +0000

    Fix eslint errors in 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,10 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  signUpUser(firstName, lastName)
-    .then((user) => uploadPhoto(fileName).then((photo) => [{ status: user.status, value: 'value or error returned by the Promise' }, { status: photo.status, value: 'value or error returned by the Promise' }]))
+  return signUpUser(firstName, lastName)
+    .then((user) => uploadPhoto(fileName).then((photo) => [
+      { status: user.status, value: user.value },
+      { status: photo.status, value: photo.value },
+    ]))
     .catch((err) => console.log(err));
 }
