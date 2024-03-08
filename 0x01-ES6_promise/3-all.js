e9fbbf9d126d72713a7f95730ccf303904495cad
Kevin Warui <waruimacharia@gmail.com>
2024-03-08 18:05:55 +0000

    Refactor 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto()
-    .then((photo) => createUser().then((user) => {
-      console.log(photo.body, user.firstName, user.lastName);
-    }))
-    .catch((err) => {
-      console.log(err);
-    });
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => { console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`); })
+    .catch(() => console.log('Signup system offline'));
 }
