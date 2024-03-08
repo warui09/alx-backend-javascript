b7d33e25b15341dcde19611147e383cea47bf502
Kevin Warui <waruimacharia@gmail.com>
2024-03-08 19:24:40 +0000

    Fix typo in npm run dev 7-main.js

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,5 +1,5 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise
-    .any([chinaDownload(), USDownload])
+    .any([chinaDownload, USDownload])
     .then((res) => res);
 }
