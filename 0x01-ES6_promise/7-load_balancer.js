b580f9480d36643e206e4ca521c40bf49d5248cd
Kevin Warui <waruimacharia@gmail.com>
2024-03-08 19:26:10 +0000

    Refactor npm run dev 7-main.js

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,5 +1,5 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise
-    .any([chinaDownload, USDownload])
+    .race([chinaDownload, USDownload])
     .then((res) => res);
 }
