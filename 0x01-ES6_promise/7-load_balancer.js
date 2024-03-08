590a52adf092eb070f30c4657470285f1e15cbd9
Kevin Warui <waruimacharia@gmail.com>
2024-03-08 19:21:53 +0000

    Add 7-load_balancer.js

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,4 +1,5 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return chinaDownload().
-    .then((chinaValue) => USDownload())
+  return Promise
+    .any([chinaDownload(), USDownload])
+    .then((res) => res);
 }
