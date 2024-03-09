bb1dd4f6640a1ac7c4fc4c1b4ed92472f549984e
Kevin Warui <waruimacharia@gmail.com>
2024-03-09 00:55:18 +0000

    Refactor 2-arrow.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,7 +1,7 @@
-module.exports = () => {
+export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  this.addNeighborhood = function add(newNeighborhood) {
+  this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
   };
