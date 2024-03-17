0907dc602a2fa95fa3e259bac5f48aa9460f3f3a
Kevin Warui <waruimacharia@gmail.com>
2024-03-17 15:57:30 +0300

    Add StudentClass to task_1/js/main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,3 +16,29 @@ export interface printTeacherFunction {
 }
 
 export const printTeacher: printTeacherFunction = (firstName, lastName) => `${firstName[0]}. ${lastName}`
+
+interface StudentClassInterface {
+  new (firstName: string, lastName: string): StudentClass;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework():string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
