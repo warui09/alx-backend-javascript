e52cf37739647f5cc5643110eabcba2c24b06de7
Kevin Warui <waruimacharia@gmail.com>
2024-04-30 13:00:54 +0000

    Refactor 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,12 +2,19 @@
  * Creates a small HTTP server using Node's HTTP module
  */
 
-const app = require('http');
+const http = require('http');
 
-app.createServer((req, res) => {
+const PORT = 1245;
+const HOST = 'localhost';
+
+const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('Hello Holberton School!');
   res.end();
-}).listen(1245);
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}/`);
+});
 
 module.exports = app;
