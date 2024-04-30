7188a230a785981f5712e08dfba26a536293710b
Kevin Warui <waruimacharia@gmail.com>
2024-04-30 08:32:19 +0000

    Add 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,22 +1,33 @@
-const fs = require('fs')
+const fs = require('fs');
 
 module.exports = function countStudents(path) {
   try {
     const db = fs.readFileSync(path, 'utf8');
     let numberStudents = 0;
-    let fields = {};
-    for (line of db.split('\n')) {
-      const lineArr = line.split(',')
+    const fields = {};
+    // eslint-disable-next-line no-plusplus
+    for (let i = 1; i < db.split('\n').length; i++) {
+      const lineArr = db.split('\n')[i].split(',');
       // check if line is valid
-      if (lineArr.length == 4) {
+      if (lineArr.length === 4) {
         // update number of students count
+        // eslint-disable-next-line no-plusplus
         numberStudents++;
 
-        //update fields and values
+        // update fields and values
+        const field = lineArr[3];
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(lineArr[0]);
       }
     }
-    console.log(`Number of students: ${numberStudents - 1}`);
+    console.log(`Number of students: ${numberStudents}`);
+    Object.entries(fields).forEach(([key, value]) => {
+      console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+    });
   } catch (e) {
-    throw "Cannot load the database";
+    // eslint-disable-next-line no-throw-literal
+    throw 'Cannot load the database';
   }
-}
+};
