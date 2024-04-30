463ce4e2863d0c375f6d5876fe6827098ed2f581
Kevin Warui <waruimacharia@gmail.com>
2024-04-30 12:47:00 +0000

    Bug fix in 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,6 +5,7 @@
 const app = require('http');
 
 app.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('Hello Holberton School!');
   res.end();
 }).listen(1245);
