8c4e4e8a8ee3be7b3ffc6bdcf58508c45b443c54
Kevin Warui <waruimacharia@gmail.com>
2024-05-01 13:45:59 +0000

    Add 0-calcul.js

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,9 @@
+/**
+ * calculates sum of two rounded numbers
+ */
+
+const calculateNumber = (a, b) => {
+  return Math.round(a) + Math.round(b);
+};
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,13 @@
+/**
+ * test function calculateNumber
+ */
+
+const assert = require('assert');
+const calculateNumber = require("./0-calcul.js");
+
+describe('Test calculateNumber', () => {
+  it('test 1, 3', () => {
+    assert.strictEqual(1 + 3, 4);
+  });
+
+});
