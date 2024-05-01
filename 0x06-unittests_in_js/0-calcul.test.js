c674ff3d3eb8132d5758161c5a75258d867c46cc
Kevin Warui <waruimacharia@gmail.com>
2024-05-01 17:33:53 +0300

    Refactor 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,13 +1,25 @@
-/**
- * test function calculateNumber
- */
-
 const assert = require('assert');
 const calculateNumber = require("./0-calcul.js");
 
-describe('Test calculateNumber', () => {
-  it('test 1, 3', () => {
-    assert.strictEqual(1 + 3, 4);
+describe('caclulateNumber', () => {
+  it('test rounding of a', () => {
+    assert.equal(calculateNumber(1.2, 1), 2);
+    assert.equal(calculateNumber(10.8, 4), 15);
+    assert.equal(calculateNumber(5.5, 2), 8);
+    assert.equal(calculateNumber(100.9, 9), 110);
+  });
+  
+  it('test rounding of b', () => {
+    assert.equal(calculateNumber(10, 1.1), 11);
+    assert.equal(calculateNumber(1, 4.4), 5);
+    assert.equal(calculateNumber(7, 7.2), 14);
+    assert.equal(calculateNumber(55, 9.9), 65);
   });
 
+  it('test sum of rounded a and b', () => {
+    assert.equal(calculateNumber(2.4, 1.1), 3);
+    assert.equal(calculateNumber(6.1, 4.4), 10);  
+    assert.equal(calculateNumber(9.7, 7.2), 17);
+    assert.equal(calculateNumber(55.6, 9.7), 66);  
+  });
 });
