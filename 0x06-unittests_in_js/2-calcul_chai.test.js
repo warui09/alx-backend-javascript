903b9e2a15392d9ab537b804fa4048caf91214b8
Kevin Warui <waruimacharia@gmail.com>
2024-05-02 11:13:26 +0300

    Add 2-calcul_chai.js and 2-calcul_chai.test.js

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  it('test SUM', () => {
+    // assert.equal(calculateNumber('SUM', 3.2, 4), 7);
+    expect(calculateNumber('SUM', 3.2, 4)).to.equal(7);
+    // assert.equal(calculateNumber('SUM', 5, 3.4), 8);
+    expect(calculateNumber('SUM', 5, 3.4)).to.equal(8);
+    assert.equal(calculateNumber('SUM', 32.9, 9.6), 43);
+  });
+
+  it('test SUBTRACT', () => {
+    assert.equal(calculateNumber('SUBTRACT', 9.1, 1), 8);
+    assert.equal(calculateNumber('SUBTRACT', 10, 10.5), -1);
+    assert.equal(calculateNumber('SUBTRACT', 7.3, 2.7), 4);
+  });
+
+  it('test DIVIDE', () => {
+    assert.equal(calculateNumber('DIVIDE', 3.3, 2), 1.5);
+    assert.equal(calculateNumber('DIVIDE', 55, 5.4), 11);
+    assert.equal(calculateNumber('DIVIDE', 39.6, 3.7), 10);
+    assert.equal(calculateNumber('DIVIDE', 9, 0.4), 'Error');
+  });
+});
+
+//expect(calculateNumber('SUM', 3.2, 4)).to.equal(7);
